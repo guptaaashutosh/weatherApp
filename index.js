2bf9cb1119f62f79d8fba040a3c69bddae5c031c
Aashutosh <guptaaashu354@gmail.com>
2022-12-02 23:38:53 +0530

    nodejs weatherApp project first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+//weather API 
+
+const http=require('http');
+const fs=require('fs');
+var requests=require('requests');
+
+const homeFile=fs.readFileSync("home.html",'utf-8');
+
+const replaceVal=(tempVal,orgVal)=>{
+    let temperature=tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature=temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature=temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature=temperature.replace("{%Location%}",orgVal.name); 
+    temperature=temperature.replace("{%country%}",orgVal.sys.country);
+    temperature=temperature.replace("{%tempStatus%}",orgVal.weather[0].main);
+    return temperature;
+}
+
+
+const server =http.createServer((req,res)=>{
+    if(req.url=="/"){
+        requests('https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=bc937d2a146365031e0f05ea01855f7a')
+         .on('data', (chunk)=>{
+            const objData=JSON.parse(chunk);
+           const arrData=[objData];
+         // console.log(arrData[0].main.temp);
+         const realTimeData=arrData.map(val=>replaceVal(homeFile,val)).join("");
+        
+        res.write(realTimeData);
+       //  console.log(realTimeData);
+      })
+      .on('end',  (err)=>{
+        if (err) return console.log('connection closed due to errors', err);
+        res.end();
+    });
+
+    }
+});
+
+server.listen(8000,"127.0.0.1");
